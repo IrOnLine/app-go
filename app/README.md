fa108581688f501c0b2fef36aaf756a9b8f418a0
IrOnLine <abro@yandex.ru>
2023-08-17 01:48:59 +0300

    new api

---
+++ app/README.md
@@ -0,0 +1,11 @@
+# Golang : Use gorm with mysql in gin
+
+This repository guides to how ORM can be implemented in Golang. After cloning the code, follow below steps to let the project run on your system.
+
+1. Run `go build main.go`
+
+2. Run `go run main.go`
+
+curl -X POST -H "Content-Type: application/json" -d '{"Name":"roman", "Email":"abro@yandex.ru"}' http://localhost:8080/users 
+
+After running the commands, you can find server started on `8080` .
--- app/main.go
+++ app/main.go
@@ -1,18 +1,29 @@
 package main
 
 import (
-	"gin.com/gin/controllers"
-	"gin.com/gin/models"
+	"app/controllers"
 	"github.com/gin-gonic/gin"
+	"net/http"
 )
 
 func main() {
-	r := gin.Default()
-	models.ConnectDatabase()
+	r := setupRouter()
+	_ = r.Run(":8080")
+}
 
-	r.GET("/api/tasks", controllers.FindTasks)
-	r.POST("/api/tasks", controllers.CreateTask)
-	r.GET("/api/tasks/one", controllers.FindTask)
-	r.PUT("/api/tasks/update", controllers.UpdateTask)
-	r.Run()
+func setupRouter() *gin.Engine {
+	r := gin.Default()
+
+	r.GET("ping", func(c *gin.Context) {
+		c.JSON(http.StatusOK, "pong")
+	})
+
+	userRepo := controllers.New()
+	r.POST("/users", userRepo.CreateUser)
+	r.GET("/users", userRepo.GetUsers)
+	r.GET("/users/:id", userRepo.GetUser)
+	r.PUT("/users/:id", userRepo.UpdateUser)
+	r.DELETE("/users/:id", userRepo.DeleteUser)
+
+	return r
 }
