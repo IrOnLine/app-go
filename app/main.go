b5e1f05cab3677f827471a0a23e97c0f754d0cf0
IrOnLine <abro@yandex.ru>
2023-08-17 01:41:08 +0300

    1

--- app/main.go
+++ app/main.go
@@ -1,54 +1,18 @@
 package main
 
 import (
-	"fmt"
-	"os"
-
+	"gin.com/gin/controllers"
+	"gin.com/gin/models"
 	"github.com/gin-gonic/gin"
-	"github.com/joho/godotenv"
-
-	"app/config"
-	"app/middleware"
-	"app/routes"
-	"app/utils"
 )
 
 func main() {
+	r := gin.Default()
+	models.ConnectDatabase()
 
-	// Load environment variables
-	if err := godotenv.Load("../.env"); err != nil {
-		fmt.Println("Error loading .env file")
-		os.Exit(1)
-	}
-
-	// Initialize logger
-	if err := utils.InitLogger(); err != nil {
-		fmt.Println("Error initializing logger")
-		os.Exit(1)
-	}
-
-	// Connect to database
-	db, err := config.ConnectDB()
-	if err != nil {
-		utils.Log.Fatal("Could not connect to database")
-		os.Exit(1)
-	}
-
-	// Initialize router
-	router := gin.Default()
-
-	// Register routes
-	routes.Register(router)
-
-	// Register middleware
-	router.Use(middleware.ErrorHandler())
-	router.Use(middleware.Logger())
-
-	// Start server
-	utils.Log.Info("Starting API server on port 8000")
-	if err := router.Run(":8000"); err != nil {
-		utils.Log.Fatal("Could not start API server")
-		os.Exit(1)
-	}
-
+	r.GET("/api/tasks", controllers.FindTasks)
+	r.POST("/api/tasks", controllers.CreateTask)
+	r.GET("/api/tasks/one", controllers.FindTask)
+	r.PUT("/api/tasks/update", controllers.UpdateTask)
+	r.Run()
 }
